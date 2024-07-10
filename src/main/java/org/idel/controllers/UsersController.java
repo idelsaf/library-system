aca39e97b9610dbf5bfb3fed91e0c97ae68d5f67
idel <little1student@gmail.com>
2024-07-10 19:06:13 +0300

    refactor: delete extra imports

--- src/main/java/org/idel/controllers/UsersController.java
+++ src/main/java/org/idel/controllers/UsersController.java
@@ -5,8 +5,6 @@ import org.idel.dao.UserDAO;
 import org.idel.models.User;
 import org.idel.util.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
