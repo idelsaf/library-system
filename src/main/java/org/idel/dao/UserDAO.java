a3d1816fef303cc44c099b4f769db29990a65a7d
idel <little1student@gmail.com>
2024-07-10 18:59:25 +0300

    feat: implement DAO for users

--- src/main/java/org/idel/dao/UserDAO.java
+++ src/main/java/org/idel/dao/UserDAO.java
@@ -1,5 +1,6 @@
 package org.idel.dao;
 
+import org.idel.models.Book;
 import org.idel.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -7,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class UserDAO {
@@ -19,5 +21,32 @@ public class UserDAO {
 
     public List<User> index() {
         return jdbcTemplate.query("SELECT * FROM users", new BeanPropertyRowMapper<>(User.class));
+    }
+
+    public User show(int id) {
+        return jdbcTemplate.query("SELECT * FROM users WHERE id=?", new Object[]{id},
+                new BeanPropertyRowMapper<>(User.class)).stream().findAny().orElse(null);
+    }
+
+    public void save(User user) {
+        jdbcTemplate.update("INSERT INTO users(name, birth_year) VALUES(?, ?)", user.getName(), user.getBirthYear());
+    }
+
+    public void update(int id, User user) {
+        jdbcTemplate.update("UPDATE users SET name=?, birth_year=? WHERE id=?", user.getName(), user.getBirthYear(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM users WHERE id=?", id);
+    }
+
+    public List<Book> getBooks(int id) {
+        return jdbcTemlate.query("SELECT * FROM books WHERE user_id=?", new Object[]{id},
+                new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Optional<User> getUserByName(String name) {
+        return jdbcTemplate.query("SELECT * FROM users WHERE name = ?", new Object[]{name},
+                new BeanPropertyRowMapper<>(User.class)).stream().findAny();
     }
 }
