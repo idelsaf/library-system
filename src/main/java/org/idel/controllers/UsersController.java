9dd2f3aa8f3fbb036d36d67aa6c759ee5226e4dc
idel <little1student@gmail.com>
2024-07-10 19:01:09 +0300

    feat: implement UserController with Validator

--- src/main/java/org/idel/controllers/UsersController.java
+++ src/main/java/org/idel/controllers/UsersController.java
@@ -1,25 +1,82 @@
 package org.idel.controllers;
 
+import jakarta.validation.Valid;
 import org.idel.dao.UserDAO;
+import org.idel.models.User;
+import org.idel.util.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
     private final UserDAO userDAO;
+    private final UserValidator userValidator;
 
     @Autowired
-    public UsersController(UserDAO userDAO) {
+    public UsersController(UserDAO userDAO, UserValidator userValidator) {
         this.userDAO = userDAO;
+        this.userValidator = userValidator;
     }
 
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("users", userDAO.index());
         return "users/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userDAO.show(id));
+        model.addAttribute("books", userDAO.getBooks(id));
+
+        return "users/show";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "users/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        userValidator.validate(user, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "users/new";
+        }
+
+        userDAO.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userDAO.show(id));
+        return "users/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        userValidator.validate(user, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "users/edit";
+        }
+
+        userDAO.update(id, user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userDAO.delete(id);
+        return "redirect:/users";
     }
 }
---
+++ src/main/java/org/idel/util/UserValidator.java
@@ -0,0 +1,32 @@
+package org.idel.util;
+
+import org.idel.dao.UserDAO;
+import org.idel.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserValidator(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        User user = (User) target;
+
+        if (userDAO.getUserByName(user.getName()).isPresent()) {
+            errors.rejectValue("name", "", "User with this name already exists");
+        }
+    }
+}
