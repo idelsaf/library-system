fc50a9e9d900d5193521b7ea765ed8e925126a38
idel <little1student@gmail.com>
2024-07-10 22:29:15 +0300

    feat: implement Controller for Books

---
+++ src/main/java/org/idel/controllers/BooksController.java
@@ -0,0 +1,72 @@
+package org.idel.controllers;
+
+import jakarta.validation.Valid;
+import org.idel.dao.BookDAO;
+import org.idel.dao.UserDAO;
+import org.idel.models.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/books")
+public class BooksController {
+    private final BookDAO bookDAO;
+
+    public BooksController(BookDAO bookDAO, UserDAO userDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("books", bookDAO.index());
+        return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.show(id));
+        model.addAttribute("user", bookDAO.getBookOwner(id));
+
+        return "books/show";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "books/new";
+        }
+
+        bookDAO.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "books/edit";
+        }
+
+        bookDAO.update(id, book);
+        return "redirect:/books";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        bookDAO.delete(id);
+        return "redirect:/books";
+    }
+}
